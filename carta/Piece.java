9239ec043acbb084284f781a28f1aef19f40a657
kenzoterada <104955407+kenzoterada@users.noreply.github.com>
2023-06-17 20:23:11 -0300

    Update Piece.java

--- carta/Piece.java
+++ carta/Piece.java
@@ -1,37 +1,50 @@
+package carta;
+
+import posicao.Color;
+
 /**
  * Classe que contém informações das peças de jogo
  */
 public class Piece {
+    private Color color;
+    private boolean isMaster;
+
     /**
      * Construtor que define a cor e o tipo da peça
-     * @param color Cor da peça
+     * 
+     * @param color    Cor da peça
      * @param isMaster Se o tipo da peça é mestre ou não
      */
     public Piece(Color color, boolean isMaster) {
+        this.color = color;
+        this.isMaster = isMaster;
 
     }
 
     /**
      * Método que devolve a cor da peça
+     * 
      * @return Enum Color com a cor da peça
      */
     public Color getColor() {
-        return null;
+        return this.color;
     }
 
     /**
      * Método que devolve se é um mestre ou não
+     * 
      * @return Booleano true para caso seja um mestre e false caso contrário
      */
     public boolean isMaster() {
-        return false;
-    }
+        return this.isMaster;
 
-    /**
-     * Método que devolve se a peça ainda está em jogo ou não
-     * @return Booleano true para caso esteja em jogo e false caso contrário
-     */
-    public boolean isAlive() {
-        return false;
+        /*
+         * if(isMaster(){
+         * return true;
+         * }
+         * else{
+         * return false;
+         * })
+         */
     }
 }
