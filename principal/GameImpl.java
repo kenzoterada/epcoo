134740b386d99127b0b93d1241e3d35b0e7a5946
kenzoterada <104955407+kenzoterada@users.noreply.github.com>
2023-06-18 17:01:04 -0300

    Update GameImpl.java

--- principal/GameImpl.java
+++ principal/GameImpl.java
@@ -6,7 +6,6 @@ import excecao.IllegalMovementException;
 import excecao.IncorrectTurnOrderException;
 import excecao.InvalidCardException;
 import excecao.InvalidPieceException;
-import jogador.Player;
 import posicao.Color;
 import posicao.Position;
 import java.util.Collections;
@@ -14,6 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GameImpl implements Game {
+    private static Card carta_no_tabuleiro;
     private Card CartaTabuleiro;
     private Player jogadorVermelho, jogadorAzul;
     private Spot[][] tabuleiro;
@@ -43,6 +43,7 @@ public class GameImpl implements Game {
         Collections.shuffle(listaCartas);
         Card[] cartas_embaralhadas = listaCartas.toArray(new Card[0]);
 
+        carta_no_tabuleiro = cartas_embaralhadas[0];
         this.tabuleiro = new Spot[5][5];
         this.CartaTabuleiro = cartas_embaralhadas[0];
         this.corJogadorTurno = CartaTabuleiro.getColor();
@@ -50,10 +51,103 @@ public class GameImpl implements Game {
         this.jogadorAzul = new Player(peca_azul, Color.BLUE, cartas_embaralhadas[3], cartas_embaralhadas[4]);
 
     }
+    // ******************************************************************************
 
-    private void definirTurno() {
-        this.corJogadorTurno = CartaTabuleiro.getColor();
+    public static Card getCarta_no_tabulerio() {
+        return carta_no_tabuleiro;
+
     }
+
+    public boolean mestre_vivo(Color cor) {
+        Piece verify;
+        for (int i = 0; i < this.tabuleiro.length; i++) {
+            for (int j = 0; j < this.tabuleiro[i].length; j++) {
+                verify = getPiece(new Position(i, j));
+
+                if (verify.isMaster() == true && verify.getColor() == cor) {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    public boolean possivel_locomover(Card card, Position cardMove) {
+        Position[] posicoes = card.getPositions();
+        for (Position posicao : posicoes) {
+            if (posicao == cardMove) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean possui_carta(Player player, Card card) {
+        Card[] cartas = player.getCards();
+        for (Card carta : cartas) {
+            if (carta == card) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Piece achar_peca(Position currentPos) {
+        Piece peca_descobrir = null;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (this.tabuleiro[i][j].getPosition() == currentPos) {
+                    peca_descobrir = tabuleiro[i][j].getPiece();
+                }
+            }
+        }
+        return peca_descobrir;
+    }
+
+    public boolean dentro_tabuleiro(Position cardMove) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (this.tabuleiro[i][j].getPosition() == cardMove) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean possui_peca_cor(Position cardMove, Color cor) {
+        if (achar_peca(cardMove).getColor() == cor) {
+            return true;
+        }
+        return false;
+    }
+
+    public Player getPlayer_pela_cor(Color cor) {
+        if (cor == Color.BLUE) {
+            return getBluePlayer();
+        } else {
+            return getRedPlayer();
+        }
+    }
+
+    public void change_turn() {
+        if (corJogadorTurno == Color.RED) {
+            corJogadorTurno = Color.BLUE;
+        } else {
+            corJogadorTurno = Color.RED;
+        }
+    }
+
+    public Position nova_posicao(Position cardMove, Position currentPos) {
+        int linha = cardMove.getRow() + currentPos.getRow();
+        int coluna = cardMove.getCol() + currentPos.getCol();
+        Position nova_posicao = new Position(linha, coluna);
+
+        return nova_posicao;
+    }
+
+    // ******************************************************************************
 
     @Override
     public Color getSpotColor(Position position) {
@@ -80,23 +174,87 @@ public class GameImpl implements Game {
         return this.jogadorAzul;
     }
 
+    // private void definirTurno() {
+    // this.corJogadorTurno = CartaTabuleiro.getColor();
+    // }
+
     @Override
     public void makeMove(Card card, Position cardMove, Position currentPos)
-            throws IncorrectTurnOrderException, IllegalMovementException, InvalidCardException, InvalidPieceException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'makeMove'");
+            throws IncorrectTurnOrderException, IllegalMovementException,
+            InvalidCardException, InvalidPieceException {
+
+        Color jogador_atual = card.getColor();
+        Player jogador = getPlayer_pela_cor(jogador_atual);
+
+        boolean no_tabuleiro = dentro_tabuleiro(cardMove);
+
+        if (this.corJogadorTurno != jogador_atual) {
+            throw new IncorrectTurnOrderException("Jogador não está no seu turno!");
+        } else if (no_tabuleiro) {
+            throw new IllegalMovementException("Movimento para fora do tabuleiro!");
+        } else if (possui_peca_cor(cardMove, jogador_atual)) {
+            throw new IllegalMovementException("Posição ja possui peça da mesma cor!");
+        } else if (!possui_carta(jogadorAzul, card)) {
+            throw new InvalidCardException("Jogador não possui carta!");
+        } else if (achar_peca(currentPos) == null) {
+            throw new InvalidPieceException("Peça não se encontra na posição atual!");
+        } else {
+            jogador.swapCard(card, carta_no_tabuleiro);
+
+            Position posicao_nova = nova_posicao(cardMove, currentPos);
+            tabuleiro[posicao_nova.getRow()][posicao_nova.getCol()].occupySpot(achar_peca(currentPos));
+            tabuleiro[currentPos.getRow()][currentPos.getCol()].releaseSpot();
+        }
     }
 
     @Override
     public boolean checkVictory(Color color) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'checkVictory'");
+        if (color == Color.BLUE) {
+            if (this.tabuleiro[4][2].getColor() == Color.BLUE ||
+                    !mestre_vivo(Color.RED)) {
+                return true;
+            }
+
+            else if (color == Color.RED) {
+                if (this.tabuleiro[0][2].getColor() == Color.RED ||
+                        !mestre_vivo(Color.BLUE)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+
     }
 
     @Override
     public void printBoard() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'printBoard'");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                Piece verifica = this.tabuleiro[i][j].getPiece();
+                if (verifica == null) {
+                    System.out.print(" e "); // empty
+                } else {
+                    boolean verifica_master = verifica.isMaster();
+                    if (verifica_master && (verifica.getColor() == Color.BLUE)) {
+                        System.out.print("M.A ");
+
+                    }
+                    if (verifica_master && (verifica.getColor() == Color.RED)) {
+                        System.out.print("M.V ");
+
+                    }
+                    if (!verifica_master && (verifica.getColor() == Color.BLUE)) {
+                        System.out.print("A ");
+
+                    }
+                    if (!verifica_master && (verifica.getColor() == Color.RED)) {
+                        System.out.print("V ");
+
+                    }
+                }
+            }
+            System.out.print("\n");
+        }
     }
 
 }
